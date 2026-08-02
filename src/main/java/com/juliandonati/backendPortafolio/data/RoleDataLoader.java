3a7bf42de30ef1ed2a53ea30bfbf1d110207248a
juliandonati <juliandonati5@gmail.com>
2026-08-02 03:24:11 -0300

    refactor: remover línea de debugging de RoleDataLoader

--- src/main/java/com/juliandonati/backendPortafolio/data/RoleDataLoader.java
+++ src/main/java/com/juliandonati/backendPortafolio/data/RoleDataLoader.java
@@ -7,10 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
